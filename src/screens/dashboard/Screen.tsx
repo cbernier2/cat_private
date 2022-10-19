da5baafc9188b54bb02bc7e05234c9e928d394af
Christian Bernier <cbernier@spiria.com>
2022-10-19 08:50:28 -0400

    Fix yarn lint

--- src/screens/dashboard/Screen.tsx
+++ src/screens/dashboard/Screen.tsx
@@ -8,19 +8,13 @@ import CatText from '../../components/text';
 import {View} from 'react-native';
 import styles from './styles';
 import CatSwitch from '../../components/switch';
-import ValuesRow from './ValuesRow';
-import CatSummaryCard from './SummaryCard';
-import HaulTruckSvg from 'assets/icons/haul_truck.svg';
-import useCatTheme from '../../hooks/useCatTheme';
 
 const DashboardScreen: React.FC<ScreenType> = () => {
   const {t} = useTranslation();
   const [loads, setLoads] = useState(false);
-  const {colors} = useCatTheme();
 
   // TODO: Retrieve from Store / API
   const siteName = 'Rasmussen Valley Clone';
-  const productionSummary = {total: 20000, project: 90000, target: 120000};
 
   return (
     <CatScreen title={t('summary_title')}>
@@ -32,7 +26,6 @@ const DashboardScreen: React.FC<ScreenType> = () => {
           onValueChange={val => setLoads(val)}
         />
       </View>
-
     </CatScreen>
   );
 };
