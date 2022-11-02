11256bf3c39e9501c23f39d7a32cc8cd5eedb3b7
Richard Martin <rmartin@spiria.com>
2022-11-02 15:29:23 -0400

    CATPS-116 Update libraries

--- src/components/pickers/time/types.ts
+++ src/components/pickers/time/types.ts
@@ -1,6 +1,12 @@
-export interface CatTimePickerType {
-  onSelect: (time: CatTimePickerSelection) => void;
-}
+import {ComponentProps} from 'react';
+import {TimePickerModal} from 'react-native-paper-dates';
+
+export type CatTimePickerType = Omit<
+  ComponentProps<typeof TimePickerModal>,
+  'onDismiss' | 'visible'
+> & {
+  children: any;
+};
 
 export interface CatTimePickerSelection {
   hours: number;
