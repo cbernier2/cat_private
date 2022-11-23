09da39d975bd991cfe5bae2f5a482a03613fb6b8
Richard Martin <rmartin@spiria.com>
2022-11-23 12:58:43 -0500

    CATPS-39 Add PageTitle component

---
+++ src/components/page-title/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 16,
+  },
+  titleIcon: {
+    marginHorizontal: 16,
+  },
+  titleText: {
+    flexShrink: 1,
+  },
+});
+
+export default styles;
