1756e7e527816173740a09b2ef7c93682ddb0071
Richard Martin <rmartin@spiria.com>
2022-11-24 11:56:36 -0500

    CATPS-239 Sort Material Legend

--- src/components/graphs/material-time-line/legend/Component.tsx
+++ src/components/graphs/material-time-line/legend/Component.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import {MaterialWithMeta} from '../types';
+
 import {LegendItem} from './legend-item/Component';
 import {LegendType} from './types';
 import {styles} from './styles';
 
 export const Legend: React.FC<LegendType> = props => {
+  const sort = (a: MaterialWithMeta, b: MaterialWithMeta) => {
+    const an = a.name!;
+    const bn = b.name!;
+
+    if (an < bn || an === 'Undefined') {
+      return -1;
+    }
+
+    if (an > bn || bn === 'Undefined') {
+      return 1;
+    }
+
+    return 0;
+  };
+
   return (
     <View style={styles.container}>
-      {props.materials.map((material, i) => (
+      {props.materials.sort(sort).map((material, i) => (
         <LegendItem
           key={`l${i}`}
           name={material.name!}
