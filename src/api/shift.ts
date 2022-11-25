f99655e9a033e65ca2e5020ac18ce0d26d5aa360
Christian Bernier <cbernier@spiria.com>
2022-11-25 12:00:50 -0500

    Remove testing code

--- src/api/shift.ts
+++ src/api/shift.ts
@@ -9,10 +9,5 @@ export const findMostRecentShift = (shifts: Shift[]): Shift[] | null => {
     .slice()
     .sort((a, b) => b.startTime - a.startTime);
 
-  const testShift = filteredShifts.find(
-    shift => shift.id === 'e48bfb2f-d7b5-4df6-8a65-b47734cff768',
-  );
-  testShift && filteredShifts.unshift(testShift);
-
   return filteredShifts.length ? filteredShifts.slice(0, 3) : null;
 };
