e2a7948bc001b600dff15fa519af36c3634ac0c3
Christian Bernier <cbernier@spiria.com>
2022-12-06 20:42:13 -0500

    CATPS-410: Set the foreground color of users avatar

--- src/components/user-banner/Component.tsx
+++ src/components/user-banner/Component.tsx
@@ -5,18 +5,28 @@ import styles from './styles';
 import {View} from 'react-native';
 import CatText from '../text';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {Person} from '../../api/types/cat/person';
 
 const CatUserBanner: React.FC<CatUserBannerType> = ({person, style}) => {
-  let avatarStyle = {backgroundColor: person?.color};
+  const avatarStyle = {
+    backgroundColor: Person.getBackgroundColour(person) ?? undefined,
+  };
+  const foregroundColor = Person.getTextColour(person);
 
   return (
     <View style={[styles.container, style]}>
       {person.initials !== undefined && person.initials.length > 0 ? (
-        <Avatar.Text size={40} label={person.initials} style={avatarStyle} />
+        <Avatar.Text
+          size={40}
+          color={foregroundColor}
+          label={person.initials}
+          style={avatarStyle}
+        />
       ) : (
         <Avatar.Icon
           size={40}
           style={avatarStyle}
+          color={foregroundColor}
           icon={props => <MaterialIcons name={'person-outline'} {...props} />}
         />
       )}
