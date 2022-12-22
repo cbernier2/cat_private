aac082e66505623e777feeb1c72db31d06c08a7c
Christian Bernier <cbernier@spiria.com>
2022-12-21 22:41:42 -0500

    Spacer component

---
+++ src/components/spacer/Component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './styles';
+import {View} from 'react-native';
+
+const CatSpacer = () => {
+  return <View style={styles.spacer} />;
+};
+
+export default CatSpacer;
---
+++ src/components/spacer/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  spacer: {
+    width: 15,
+    height: 15,
+  },
+});
+
+export default styles;
